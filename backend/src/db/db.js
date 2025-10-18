3fe7972f9183c52b2b5d2980e90503ddac9bdc66
KunalChoudhary03 <kunalchoudhary3344@gmail.com>
2025-10-18 10:52:00 +0530

    Added

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
-async function connectDb(){
 try{
     await mongoose.connect("mongodb+srv://kunalchoudhary_03:of732AOr2xl94IsY@kunalchoudhary03.hgnetyf.mongodb.net/Database")
-    console.log('Connected to database');
+    console.log("Connected to DataBase");
 }
 catch(error){
     console.log('Error',error);
+    
 }
-}
-module.exports =  connectDb
