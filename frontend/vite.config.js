6a53664136bfbb83be4c82cc96917ac544e6215a
KunalChoudhary03 <kunalchoudhary3344@gmail.com>
2025-12-22 22:08:42 +0530

    Fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 5173, // Frontend port - Backend is on 3000
+    port: 5173,
+    historyApiFallback: true, 
   },
   plugins: [react(),
      tailwindcss(),
