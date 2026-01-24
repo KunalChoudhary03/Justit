d8bf8f3437f21ef0922a6cbeebf624efa285f8cb
KunalChoudhary03 <kunalchoudhary3344@gmail.com>
2026-01-24 21:46:56 +0530

    Fixed

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,8 @@
-require('dotenv').config();
 const app = require('./src/app.js');
 const connectDb = require('./src/db/db.js');
-const axios = require('axios');
 
-const PORT = process.env.PORT || 3000;
-const url = 'https://justit-yyzo.onrender.com/';
-const interval = 30000;
+connectDb();
 
-function reloadWebsite() {
-  axios.get(url)
-    .then(() => console.log("Website reloaded"))
-    .catch(err => console.error("Ping error:", err.message));
-}
-
-setInterval(reloadWebsite, interval);
-
-(async () => {
-  try {
-    await connectDb();
-    app.listen(PORT, () =>
-      console.log(`Server running on port ${PORT}`)
-    );
-  } catch (err) {
-    console.error("DB connection failed", err);
-  }
-})();
+app.listen(3000,()=>{
+  console.log("Server is running on port 3000");
+})
